03ab9ec0f05e4bcf76e4bd4f7a029a14418a3c08
SamuelLazurca <samilazurca15@gmail.com>
2026-01-13 05:56:55 +0200

    wiki

---
+++ scholarly-html/js/refs.js
@@ -0,0 +1,18 @@
+
+let refs = {
+  'WADE': `
+  <dd property="schema:citation" typeof="schema:ScholarlyArticle"
+      resource="https://profs.info.uaic.ro/sabin.buraga/teach/courses/wade/web-film.html">
+    <cite property="schema:name"><a href="https://profs.info.uaic.ro/sabin.buraga/teach/courses/wade/web-film.html">The
+      WADE Syllabus</a></cite>
+  </dd>
+  `,
+
+  'schema.org': `
+  <dd property="schema:citation" typeof="schema:WebPage"
+      resource="http://schema.org/">
+    <cite property="schema:name"><a href="http://schema.org/">schema.org</a></cite>.
+  </dd>
+  `,
+};
+export default refs;
